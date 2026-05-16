6d3001e9c28cfb548887fc5671ce8d450da0af4b
João Gabriel Borba do Nascimento <108697751+joaogabriel43@users.noreply.github.com>
2026-05-16 19:23:05 -0300

    chore: estruturar pacotes da clean architecture

---
+++ src/main/java/com/postmortemai/PostMortemAiApplication.java
@@ -0,0 +1,12 @@
+package com.postmortemai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostMortemAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostMortemAiApplication.class, args);
+    }
+}
